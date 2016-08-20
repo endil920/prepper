064c4e2964dc68371e912af276550c4794f457ad
Dillon Eng <dillon@MacBook-Pro.local>
2016-08-20 16:53:07 -0700

    use process.env.PORT

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/verse/:query', function(req, res) {
     });
 
 });
-
-app.listen(3000, function() {
-    console.log('Up and running on port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Up and running on port ' + port);
 });
